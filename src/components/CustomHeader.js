847ea5712c905d30899853c164ba77145beb0190
bsh04 <dmitry_vl98@mail.ru>
2020-11-15 15:26:22 +0700

    refactor#3

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -5,6 +5,11 @@ export const CustomHeader = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to='/'>Учет товаров</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
+            </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
